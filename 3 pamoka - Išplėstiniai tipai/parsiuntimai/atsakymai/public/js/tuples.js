c57b17ddf14f1517860c75c8176f53d603f498e5
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-24 21:38:42 +0200

    feat(3): tuples exerciss solved

--- 3 pamoka - Išplėstiniai tipai/parsiuntimai/atsakymai/public/js/tuples.js
+++ 3 pamoka - Išplėstiniai tipai/parsiuntimai/atsakymai/public/js/tuples.js
@@ -13,14 +13,32 @@ console.group('Tuples - užduotys');
 {
     console.group('1. Sukurkite žmogaus vardo ir amžiaus rinkinį');
     {
+        const person1 = ['Kerpis', 12];
+        const person2 = ['Kiaunė', 24];
+        console.log(person1, person2);
     }
     console.groupEnd();
     console.group('2. Sukurkite šuns šeimininko ir šuns rinkinį(pradžioje teks sukurti Šuns ir Šeiminiko tipus)');
     {
+        const friendship1 = [{
+                name: 'Agnolis',
+                age: 12
+            }, {
+                name: 'Barkis',
+                age: 2,
+                breed: 'Dalmantin'
+            }];
+        console.log(friendship1);
     }
     console.groupEnd();
     console.group('3. Sukurkite trikampio taškų 2D erdvėje rinkinį(pradžioje teks sukurti 2D Taško tipą)');
     {
+        const triangle1 = [[-2, 1], [2, 2], [3, 1]];
+        const rightTriangle = [[0, 0], [4, 0], [4, 3]];
+        console.log({
+            triangle1,
+            rightTriangle,
+        });
     }
     console.groupEnd();
 }
--- 3 pamoka - Išplėstiniai tipai/parsiuntimai/atsakymai/src/tuples.ts
+++ 3 pamoka - Išplėstiniai tipai/parsiuntimai/atsakymai/src/tuples.ts
@@ -20,24 +20,54 @@ const height: UseNumericStateTuple = [currentAge, (h) => {
 console.group('Tuples - užduotys');
 {
   // ↓↓↓↓ Tipus apraškite čia ↓↓↓↓
-
+  type PersonTuple = [string, number];
+  type Dog = {
+    name: string,
+    age: number,
+    breed: 'Dalmantin' | 'Tax',
+  };
+  type Person = {
+    name: string,
+    age: number,
+  };
+  type FriendshipTuple = [Person, Dog];
+  type PointTuple = [number, number];
+  type TriangleTuple = [PointTuple, PointTuple, PointTuple];
   // ↑↑↑↑ Tipus apraškite čia ↑↑↑↑
 
   console.group('1. Sukurkite žmogaus vardo ir amžiaus rinkinį');
   {
-    // sprendimo pavyzdžius spausdinkite čia 
+    const person1: PersonTuple = ['Kerpis', 12];
+    const person2: PersonTuple = ['Kiaunė', 24];
+
+    console.log(person1, person2);
   }
   console.groupEnd();
 
   console.group('2. Sukurkite šuns šeimininko ir šuns rinkinį(pradžioje teks sukurti Šuns ir Šeiminiko tipus)');
   {
-    // sprendimo pavyzdžius spausdinkite čia 
+    const friendship1: FriendshipTuple = [{
+      name: 'Agnolis',
+      age: 12
+    }, {
+      name: 'Barkis',
+      age: 2,
+      breed: 'Dalmantin'
+    }];
+
+    console.log(friendship1);
   }
   console.groupEnd();
 
   console.group('3. Sukurkite trikampio taškų 2D erdvėje rinkinį(pradžioje teks sukurti 2D Taško tipą)');
   {
-    // sprendimo pavyzdžius spausdinkite čia 
+    const triangle1: TriangleTuple = [[-2, 1], [2, 2], [3, 1]];
+    const rightTriangle: TriangleTuple = [[0, 0], [4, 0], [4, 3]];
+
+    console.log({
+      triangle1,
+      rightTriangle,
+    });
   }
   console.groupEnd();
 
