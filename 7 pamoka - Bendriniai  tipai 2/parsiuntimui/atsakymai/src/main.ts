26ca29d5432277096c12aa3ea9f73da1b98c995b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-02 11:36:13 +0200

    feat(7, 3): added tasks and fixed incorrect type

--- 7 pamoka - Bendriniai  tipai 2/parsiuntimui/atsakymai/src/main.ts
+++ 7 pamoka - Bendriniai  tipai 2/parsiuntimui/atsakymai/src/main.ts
@@ -1,3 +1,30 @@
-// 1. Sukurkitę sąrašo mazgo struktūrą, bet kokiam duomenų tipui 
-// 2. 
+// 1. Sukurkitę sąrašo mazgo struktūrą ListNode, bet kokiam duomenų tipui 
+type ListNode<T> = {
+  data: T,
+  next: ListNode<T>
+};
+
+// 2. Sukurkite sąrašo klasę List.
+//    Sukurkite konstruktorių, jog jis galėtų priimti pirmu parametru perduotą ListNode elementą ir priskirti
+//    jį į sąrašo pradžios savybė -> head ir sąrašo pabaigos savybę -> tail.
+//    Taip pat įgalinkite, kad konstruktoriaus mazgo parametras nebūtų privalomas.
+class List<T> {
+  public head: ListNode<T> | null;
+  public tail: ListNode<T> | null;
+
+  constructor(initialNode?: ListNode<T>) {
+    if (initialNode !== undefined) {
+      this.head = initialNode;
+      this.tail = initialNode;
+    }
+    else {
+      this.head = null;
+      this.tail = null;
+    }
+  }
+}
+
+// 3. Sukurkite metodą pridėti sąrašo elementui. 
+//    Elementai visuomet pridedami į sąrašo galą, t.y.: paskutinis
+
 
