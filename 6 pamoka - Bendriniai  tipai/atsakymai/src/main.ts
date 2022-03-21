67e948d3bff6e7f99f1d3b4148250576973857f9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-21 07:39:32 +0200

    refactor: restructured lessons

--- 6 pamoka - Bendriniai  tipai/atsakymai/src/main.ts
+++ 6 pamoka - Bendriniai  tipai/atsakymai/src/main.ts
@@ -1,12 +1,20 @@
 type PrimitiveType = string | number | boolean;
 
+/*
+  Šių pratybų tikslas su išspręsti užduotis panaudojant bendrinius tipus. [1-6]
+  Funkcijų parametrai turi būti bendrinio tipo/ų, pagal kurios būtų suformuojami atsakymai
+
+  7 užduotis, skirta savarankiškai išmokti patikrinti tipus:
+  https://www.typescriptlang.org/docs/handbook/advanced-types.html#user-defined-type-guards
+*/
+
 const numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
 const strings: string[] = ["pirmadienis", "antradienis", "trečiadienis", "ketvirtadienis", "penktadienis", "šeštadienis", "sekmadienis"];
 const booleans: boolean[] = [true, true, true, true, false];
 
 console.group('1. Parašykite funkciją, kuri grąžina pirmą masyvo elementą.');
 {
-  function solution<Type>(arr: Type[]): Type | undefined {
+  const solution = <Type>(arr: Type[]): Type | undefined => {
     return arr[0];
   }
 
@@ -18,7 +26,7 @@ console.groupEnd();
 
 console.group('2. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
 {
-  function solution<Type>(arr: Type[]): Type | undefined {
+  const solution = <Type>(arr: Type[]): Type | undefined => {
     return arr[arr.length - 1];
   }
 
@@ -30,8 +38,9 @@ console.groupEnd();
 
 console.group('3. Parašykite funkciją, kuri grąžina vienarūšių primityvių reikšmių masyvo kopiją');
 {
-  function solution<Type extends PrimitiveType>(arr: Type[]): Type[] {
+  const solution = <Type extends PrimitiveType>(arr: Type[]): Type[] => {
     const copy: Type[] = arr.map(x => x);
+
     return copy;
   }
 
@@ -46,13 +55,11 @@ console.group('4. Parašykite funkciją,  kuri pirmu parametru priima string | n
   // ('a', 2) -> ['a', 'a']
   // (77, 4) -> [77, 77, 77, 77]
   // (true, 1) -> [true]
+  type ArgumentSample = [PrimitiveType, number];
 
-  function solution<T extends PrimitiveType>(value: T, count: number): Array<T> {
+  const solution = <T extends PrimitiveType>(value: T, count: number): Array<T> => {
     return Array.from(new Array(count)).map(_ => value);
   }
-
-  // Spausdinimas
-  type ArgumentSample = [PrimitiveType, number];
 
   const dataSamples: ArgumentSample[] = [
     ['a', 2],
@@ -71,12 +78,11 @@ console.groupEnd();
 
 console.group('5. Parašykite funkciją, kuri sujungia tokių pat tipų masyvus į vieną masyvą');
 {
-  function solution<Type>(arr1: Type[], arr2: Type[]): Type[] {
+  type ArgumentSample<T> = [T[], T[]];
+
+  const solution = <Type>(arr1: Type[], arr2: Type[]): Type[] => {
     return [...arr1, ...arr2];
   }
-
-  // Spausdinimas
-  type ArgumentSample<T> = [T[], T[]];
 
   const args1: ArgumentSample<number> = [[1, 2, 3], [4, 5, 6]];
   const args2: ArgumentSample<string> = [['labas', 'mano', 'vardas'], ['yra', 'ponas', 'krabas']];
@@ -96,8 +102,9 @@ console.group('6. Parašykite funkciją, kuri priimtų bet kokią reikšmę ir g
   };
 
 
-  function solution<Type>(initialValue: Type): IncapsulatedValueObject<Type> {
+  const solution = <Type>(initialValue: Type): IncapsulatedValueObject<Type> => {
     let value: Type = initialValue;
+
     return {
       setValue: (newValue) => value = newValue,
       getValue: () => value,
@@ -126,3 +133,77 @@ console.group('6. Parašykite funkciją, kuri priimtų bet kokią reikšmę ir g
   obj3.setValue({ name: 'Pakaitalas', surname: 'Fuflo' });
 }
 console.groupEnd();
+
+console.group(`
+  7. Turite 2 tipus: Student ir Worker kurie pasižymi bendrais bruožais Person. 
+  Parašykite 2 funkcijas <isStudent> ir <isWorker> skirtas atpažinti koks objektas buvo perduotas.
+  Sukūrę tokias funkcijas iteruokite per žmonių masyvą, sugrupuodami elementus pagal tipą`
+);
+// Oficialus būdas patikrinti tipą
+// https://www.typescriptlang.org/docs/handbook/advanced-types.html#user-defined-type-guards
+{
+  type Person = {
+    name: string,
+    surname: string,
+  };
+
+  type Student = Person & {
+    university: string,
+    course: number,
+  };
+
+  type Worker = Person & {
+    avgMonthlyPay: number,
+  };
+
+  type GroupedPeople = {
+    people: Person[],
+    students: Student[],
+    workers: Worker[],
+  };
+
+  const isWorker = (person: Person): person is Worker => {
+    return (person as Worker).avgMonthlyPay !== undefined;
+  }
+
+  const isStudent = (person: Person): person is Student => {
+    const student = person as Student;
+
+    return student.university !== undefined && student.course !== undefined;
+  }
+
+  const solution = (people: Person[]): GroupedPeople => {
+    const groupedPeople = people.reduce<GroupedPeople>((prevGroupedPeople, person) => {
+      const newGroupedPeople = { ...prevGroupedPeople };
+
+      if (isWorker(person)) newGroupedPeople.workers.push(person);
+      if (isStudent(person)) newGroupedPeople.students.push(person);
+      else newGroupedPeople.people.push(person);
+
+      return newGroupedPeople;
+    }, {
+      people: [],
+      students: [],
+      workers: [],
+    });
+
+    return groupedPeople;
+  }
+
+  const people: (Person | Student | Worker)[] = [
+    { name: 'Atstovė', surname: 'Galtokaitė', university: 'VU', course: 2 },
+    { name: 'Kurpius', surname: 'Medainis' },
+    { name: 'Varnas', surname: 'Akilaitis', avgMonthlyPay: 2000 },
+    { name: 'Ferodijus', surname: 'Cilcius' },
+    { name: 'Sobora', surname: 'Kupolaityė', avgMonthlyPay: 1000 },
+    { name: 'Zubrius', surname: 'Sulindauskas', university: 'VU', course: 2 },
+    { name: 'Šidelė', surname: 'Gyslovienė', avgMonthlyPay: 1500 },
+    { name: 'Užuodauskas', surname: 'Perrašimauskas', university: 'VGTU', course: 1 },
+  ];
+
+  // (Person | Student | Worker)[] === Person[] ????
+  // https://www.javatpoint.com/typescript-duck-typing
+  const groupedPeople = solution(people);
+
+  console.log(groupedPeople);
+}
